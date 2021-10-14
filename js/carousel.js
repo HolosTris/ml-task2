5628a89bbadc9c0b2695f774aa169b9ef7dbd6ec
HolosTris <sasha.triseykin@mail.ru>
2021-10-14 14:54:11 +0300

    Добавил JSON-файл со списком номеров в отеле. Сделал отображение этого списка в каталоге. Переделал рандомную генерацию карточек.

--- js/carousel.js
+++ js/carousel.js
@@ -1,119 +1,118 @@
-const allCarousels = $('.carousel-wrap');
+loadedCatalog.finally(() => {
+	const allCarousels = $('.carousel-wrap');
 
-	$(document).ready(function(){
-for (let carousel of allCarousels) {
-	//number of slides
-	const totalSlides = $('ul li', carousel).length;
-	//get the slide width
-	const sliderWidth = $(carousel).width();
-	//current position
-	let pos = 0;
+		$(document).ready(function(){
+	for (let carousel of allCarousels) {
+		//number of slides
+		const totalSlides = $('ul li', carousel).length;
+		//get the slide width
+		const sliderWidth = $(carousel).width();
+		//current position
+		let pos = 0;
 
-	console.log(totalSlides);
-	console.log($(carousel).width());
-
-	// Carousel
+		// Carousel
 
 
 
-		/*****************
-		 BUILD THE SLIDER
-		*****************/
-		//set width to be 'x' times the number of slides
-		$('ul.carousel', carousel).width(sliderWidth*totalSlides);
+			/*****************
+			 BUILD THE SLIDER
+			*****************/
+			//set width to be 'x' times the number of slides
+			$('ul.carousel', carousel).width(sliderWidth*totalSlides);
 
-			//next slide
-		$('#next', carousel).click(function(){
-			slideRight();
-		});
+				//next slide
+			$('#next', carousel).click(function(){
+				slideRight();
+			});
 
-		//previous slide
-		$('#previous', carousel).click(function(){
-			slideLeft();
-		});
+			//previous slide
+			$('#previous', carousel).click(function(){
+				slideLeft();
+			});
 
 
 
-		/*************************
+			/*************************
+			 //*> OPTIONAL SETTINGS
+			************************/
+			//automatic slider
+			// var autoSlider = setInterval(slideRight, 3000);
+
+			//for each slide
+			$.each($('ul li', carousel), function() {
+				//set its color
+				// var c = $(this).attr("data-color");
+				// $(this).css("background",c);
+
+				//create a pagination
+				var li = document.createElement('li');
+				$('#pagination-wrap ul', carousel).append(li);
+			});
+
+			//counter
+			// countSlides();
+
+			//pagination
+			pagination();
+
+			//hide/show controls/btns when hover
+			//pause automatic slide when hover
+			// $(carousel).hover(
+			//   function(){ $(this).addClass('active'); clearInterval(autoSlider); },
+			//   function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+			// );
+
+			$(carousel).hover(
+				function(){ $(this).addClass('active'); },
+				function(){ $(this).removeClass('active'); }
+			);
+
+
+
+
+
+
+		/***********
+		 SLIDE LEFT
+		************/
+		function slideLeft(){
+			pos--;
+			if(pos==-1){ pos = totalSlides-1; }
+			$('ul.carousel', carousel).css('left', -(sliderWidth*pos));
+
+			//*> optional
+			// countSlides();
+			pagination();
+		}
+
+
+		/************
+		 SLIDE RIGHT
+		*************/
+		function slideRight(){
+			pos++;
+			if(pos==totalSlides){ pos = 0; }
+			$('ul.carousel', carousel).css('left', -(sliderWidth*pos));
+
+			//*> optional
+			// countSlides();
+			pagination();
+		}
+
+
+
+
+		/************************
 		 //*> OPTIONAL SETTINGS
 		************************/
-		//automatic slider
-		// var autoSlider = setInterval(slideRight, 3000);
+		// function countSlides(){
+		// 	$('#counter').html(pos+1 + ' / ' + totalSlides);
+		// }
 
-		//for each slide
-		$.each($('ul li', carousel), function() {
-			//set its color
-			// var c = $(this).attr("data-color");
-			// $(this).css("background",c);
-
-			//create a pagination
-			var li = document.createElement('li');
-			$('#pagination-wrap ul', carousel).append(li);
-		});
-
-		//counter
-		// countSlides();
-
-		//pagination
-		pagination();
-
-		//hide/show controls/btns when hover
-		//pause automatic slide when hover
-		// $(carousel).hover(
-		//   function(){ $(this).addClass('active'); clearInterval(autoSlider); },
-		//   function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
-		// );
-
-		$(carousel).hover(
-			function(){ $(this).addClass('active'); },
-			function(){ $(this).removeClass('active'); }
-		);
-
-
-
-
-
-
-	/***********
-	 SLIDE LEFT
-	************/
-	function slideLeft(){
-		pos--;
-		if(pos==-1){ pos = totalSlides-1; }
-		$('ul.carousel', carousel).css('left', -(sliderWidth*pos));
-
-		//*> optional
-		// countSlides();
-		pagination();
+		function pagination(){
+			$('#pagination-wrap ul li', carousel).removeClass('active');
+			$('#pagination-wrap ul li:eq('+pos+')', carousel).addClass('active');
+		}
 	}
-
-
-	/************
-	 SLIDE RIGHT
-	*************/
-	function slideRight(){
-		pos++;
-		if(pos==totalSlides){ pos = 0; }
-		$('ul.carousel', carousel).css('left', -(sliderWidth*pos));
-
-		//*> optional
-		// countSlides();
-		pagination();
-	}
-
-
-
-
-	/************************
-	 //*> OPTIONAL SETTINGS
-	************************/
-	// function countSlides(){
-	// 	$('#counter').html(pos+1 + ' / ' + totalSlides);
-	// }
-
-	function pagination(){
-		$('#pagination-wrap ul li', carousel).removeClass('active');
-		$('#pagination-wrap ul li:eq('+pos+')', carousel).addClass('active');
-	}
-}
-});//DOCUMENT READY
+	});//DOCUMENT READY
+});
---
+++ js/catalog.js
@@ -0,0 +1,97 @@
+const catalog = document.querySelector(".catalog");
+
+const loadedCatalog = fetch("/json/hotel_rooms.json")
+  .then(response => response.json())
+  .catch(error => alert(error))
+  .then(rooms => {
+    for (let room of rooms) {
+      createCard(room);
+    }
+    
+    createRandomCatalog(6);
+  });
+
+function createCard(info) {
+  catalog.insertAdjacentHTML("beforeend", `
+    <article>
+      <div class="carousel-wrap">
+        <a href="" class="card-link">
+          <ul class="carousel">
+          <--images-->
+          </ul>
+        </a>
+        <div class="btns" id="next"><i class="material-icons">expand_more</i></div>
+        <div class="btns" id="previous"><i class="material-icons">expand_more</i></div>
+        <div id="counter"></div>
+
+        <div id="pagination-wrap">
+          <ul>
+          </ul>
+        </div>
+      </div>
+      <a href="" class="card-link">
+        <div class="info">
+          <span class="room-number"><h2><span>№</span> ${info.number}</h2><h3>${(info.isSuite)? "люкс" : ""}</h3></span>
+          <span class="room-price"><b>${beutifyNumber(info.price)}₽</b> в сутки</span>
+          <div class="line"></div>
+          <span class="room-rating material-icons"></span>
+          <span class="room-comments"><b>${info.comments}</b> Отзывов</span>
+        </div>
+      </a>
+    </article>`);
+  
+  const card = catalog.querySelectorAll("article")[catalog.querySelectorAll("article").length - 1];
+
+  for (let i = 1; i <= 5; i++)
+    if (i <= info.rating) card.querySelector(".room-rating").innerHTML += "<span>star</span>";
+    else card.querySelector(".room-rating").innerHTML += "<span>star_border</span>";
+
+    
+  card.querySelector(".carousel").innerHTML = "";
+  for (image of info.images) {
+    card.querySelector(".carousel").insertAdjacentHTML("beforeend", `
+      <li>
+        <img src="${image}" alt="">
+      </li>`);
+  }
+}
+
+function createRandomCard() {
+  const randomInfo = {
+    "number": Math.ceil(Math.random() * 999),
+    "price": Math.round(Math.random() * 99) * 100 + 90,
+    "rating": Math.ceil(Math.random() * 3 + 2),
+    "comments": Math.round(Math.random() * 200 + 50),
+    "isSuite": (Math.random() < 0.5)? true : false,
+    "images": []
+  }
+  const numImages = Math.ceil(Math.random() * 5 + 1);
+  
+  for (let i = 0; i < numImages; i++)
+    randomInfo.images.push(`images/catalog/catalog${Math.ceil(Math.random() * 12)}.png`);
+
+  createCard(randomInfo);
+}
+
+function createRandomCatalog(amount = 1) {
+  for (let i = 0; i < amount; i++) createRandomCard();
+}
+
+function beutifyNumber(number = 1, separator = " ") {
+  let numStr = String(number);
+  let fractionalPart = "";
+
+  if (typeof number === "number" && !Number.isInteger(number)) {
+    const dotI = numStr.indexOf(".");
+    fractionalPart = numStr.slice(dotI);
+    numStr = numStr.slice(0, dotI);
+  }
+
+  // console.log(number);
+  
+  if (numStr.length > 3)
+    return numStr = beutifyNumber(numStr.slice(0, -3)) + separator + numStr.slice(-3) + fractionalPart;
+  else return numStr;
+}
+
+// console.log(beutifyNumber(21349123.4324));
